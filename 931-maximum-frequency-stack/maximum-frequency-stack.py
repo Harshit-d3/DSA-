c0e618882769fef49a794f9129308b5724751949
Harshit-d3 <hdangwal31204@gmail.com>
2026-07-30 20:30:10 +0530

    Time: 71 ms (71.29%) | Memory: 27.9 MB (34.65%) - LeetSync

---
+++ 931-maximum-frequency-stack/maximum-frequency-stack.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+class FreqStack:
+
+    def __init__(self):
+        self.freq = defaultdict(int)      # value -> frequency
+        self.group = defaultdict(list)    # frequency -> stack of values
+        self.maxFreq = 0
+
+    def push(self, val: int) -> None:
+        self.freq[val] += 1
+        f = self.freq[val]
+
+        if f > self.maxFreq:
+            self.maxFreq = f
+
+        self.group[f].append(val)
+
+    def pop(self) -> int:
+        val = self.group[self.maxFreq].pop()
+        self.freq[val] -= 1
+
+        if not self.group[self.maxFreq]:
+            self.maxFreq -= 1
+
+        return val
